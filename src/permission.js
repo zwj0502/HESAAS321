73a3b411a115c1a3fe6ad5f87f15c19558d7780a
ZhangWJ <2609082088@qq.com>
2022-09-26 11:02:26 +0800

    feat:123

---
+++ src/permission.js
@@ -0,0 +1,27 @@
+import router from "@/router";
+import store from '@/store'
+const wheteelist = ['/login', '/404']
+// 任何一个路由跳转 都会进入这个全局前置守卫
+router.beforeEach((to, from, next) => {
+     // ...
+  // to 将要进入的目标
+  // from 当前导航正要离开的路由
+  // next  Function 
+  // next() 意味着都 当前这个钩子 结束 进入下一个
+  // next(false) 中断当前的导航
+  // next('/') 或者 next({ path: '/' })  跳转到一个不同的地址   
+  //  当前的导航被中断，然后进行一个新的导航
+    if (store.getters.token) {
+        if (to.path === '/login') {
+            next('/')
+        } else {
+            next()
+        }
+    } else { 
+        if (wheteelist.includes(to.path)) {
+            next()
+        } else { 
+            next('/login')
+        }
+    }
+})
--- src/utils/request.js
+++ src/utils/request.js
@@ -5,15 +5,20 @@ const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API,
   timeout: 5000
 })
-service.interceptors.response.use(response => { 
-  const { message, data, success } = response.data
-  if (success) { 
-return data
+// 响应拦截器
+service.interceptors.response.use(response => {
+  // axios默认加了一层data
+  const { success, message, data } = response.data
+  //   要根据success的成功与否决定下面的操作
+  if (success) {
+    return data
+  } else {
+    // 业务已经错误了 还能进then ? 不能 ！ 应该进catch
+    Message.error(message) // 提示错误消息
+    return Promise.reject(new Error(message))
   }
-  Message.error(message)
-  return Promise.reject(new Error(message))
-}, error => { 
-  Message.error(error.message)
-  return Promise.reject(error)
+}, error => {
+  Message.error(error.message) // 提示错误信息
+  return Promise.reject(error) // 返回执行错误 让当前的执行链跳出成功 直接进入 catch
 })
 export default service
